8109178daa29b7b5b2e1f78ddc1500768a5901c1
sktgithubl <skt.sipun123@gmail.com>
2021-09-11 12:28:13 +0530

    retrieving all courses test added

---
+++ src/test/java/com/example/jpahibernate/repository/CriteriaQueryTest.java
@@ -0,0 +1,53 @@
+package com.example.jpahibernate.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.jpahibernate.JpaHibernateApplication;
+import com.example.jpahibernate.entity.Course;
+import com.example.jpahibernate.entity.Student;
+
+@SpringBootTest(classes=JpaHibernateApplication.class)
+public class CriteriaQueryTest {
+	
+	Logger logger = LoggerFactory.getLogger(CriteriaQueryTest.class);
+	
+	@Autowired
+	EntityManager em;
+	
+	@Test
+	public void jpql_basic() {
+		//Select c from Course c
+		
+		// 1. Use Criteria builder to create a Criteria Query returning the
+		// expected result object.
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+		
+		// 2. Define roots for tables which are involved in the query
+		Root<Course> courseRoot = cq.from(Course.class);
+		
+		// 3. Define Predicated etc using Criteria Builder
+		
+		// 4. Add Predicates etc to the Criteria Query
+		
+		// 5. Build the TypedQuery using the entity manager and criteria query 
+		TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+		List<Course> resultList = query.getResultList();
+		
+		logger.info("jpql Query: select c form Course c -> {}", resultList);
+		return;
+	}
+}
